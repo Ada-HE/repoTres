adb1bb1fed7b19f70165b42fd908d1233441b0a3
Ada-HE <20221018@uthh.edu.mx>
2026-07-03 13:06:21 -0600

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="repoTres",                                # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Ada",                                   # Tu nombre
+    author_email="dairworking@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/Ada-HE/repoTres",     # URL del proyecto
+)
